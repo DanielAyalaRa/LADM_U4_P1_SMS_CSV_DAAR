e6a6933607ec3f9ae4d836e384a58c65c9ec64f6
DanielAyalaRa <80473383+DanielAyalaRa@users.noreply.github.com>
2022-05-30 00:31:28 -0600

    abrir archivo excel

--- app/src/main/java/mx/edu/ittepic/daar/ladm_u4_p1_daar/MainActivity2.kt
+++ app/src/main/java/mx/edu/ittepic/daar/ladm_u4_p1_daar/MainActivity2.kt
@@ -2,24 +2,30 @@ package mx.edu.ittepic.daar.ladm_u4_p1_daar
 
 import android.R
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.FileProvider
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.getValue
 import mx.edu.ittepic.daar.ladm_u4_p1_daar.databinding.ActivityMain2Binding
-import java.io.IOException
-import java.io.OutputStreamWriter
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
+import java.io.*
+import java.lang.StringBuilder
 
 class MainActivity2 : AppCompatActivity() {
     lateinit var binding: ActivityMain2Binding
     var listaIDs = ArrayList<String>()
     var csv = ArrayList<Dato>()
+    var listacsv = ArrayList<Dato>()
     var datos = ArrayList<String>()
     var listaSMSSeleccionados = ArrayList<String>()
 
@@ -74,23 +80,15 @@ class MainActivity2 : AppCompatActivity() {
         }
 
         binding.botonCSV.setOnClickListener {
-            if (listaSMSSeleccionados.size == null) {
+            if (listaSMSSeleccionados.isEmpty()) {
                 Toast.makeText(this, "LISTA VACIA",Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
-            var lista = "Telefono,Mensaje,Fecha y hora\n"
+            crearCsvDocumento(csv)
+        }
 
-            (0..listaSMSSeleccionados.size-1).forEach { 
-                lista+=listaSMSSeleccionados.get(it)+"\n"
-            }
-            try {
-                val archivo = OutputStreamWriter(openFileOutput("archivo.csv", Activity.MODE_PRIVATE))
-                archivo.write(lista)
-                archivo.flush()
-                archivo.close()
-            } catch (e: IOException) { }
-            Toast.makeText(this, "Los datos fueron grabados",Toast.LENGTH_SHORT).show()
-            finish()
+        binding.abrirArchivo.setOnClickListener {
+            openCsvDocument()
         }
     }
 
@@ -102,16 +100,91 @@ class MainActivity2 : AppCompatActivity() {
             AlertDialog.Builder(this)
                 .setTitle("GUARDAR")
                 .setMessage("¿Desea agregar el indice ${posicion}?")
-                .setPositiveButton("AGREGAR", {d,i->
+                .setPositiveButton("AGREGAR") { d, i ->
                     listaSMSSeleccionados.add("${csv[posicion].telefono},${csv[posicion].mensaje},${csv[posicion].fecha}")
+                    listacsv.add(
+                        Dato(
+                            csv[posicion].telefono,
+                            csv[posicion].mensaje,
+                            csv[posicion].fecha
+                        )
+                    )
 
-                    binding.listaCVS.adapter = ArrayAdapter<String>(this, R.layout.simple_expandable_list_item_1,listaSMSSeleccionados)
+                    binding.listaCVS.adapter = ArrayAdapter<String>(
+                        this,
+                        R.layout.simple_expandable_list_item_1,
+                        listaSMSSeleccionados
+                    )
                     d.dismiss()
-                })
-                .setNeutralButton("CANCELAR", {d,i->
+                }
+                .setNeutralButton("CANCELAR") { d, i ->
                     d.cancel()
-                })
+                }
                 .show()
+        }
+    }
+
+    private fun crearCsvDocumento(smsMessageList : ArrayList<Dato>) {
+        val path = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_DOCUMENTS
+        )
+
+        val fileName = "archivo.csv"
+        val file = File(path, "/$fileName")
+        Toast.makeText(this,"${path}",Toast.LENGTH_SHORT).show()
+
+        writeToCsvFile(file, smsMessageList)
+    }
+
+    private fun writeToCsvFile(file : File, smsMessageList : ArrayList<Dato>) {
+        val writer = BufferedWriter(file.bufferedWriter());
+
+        val csvPrinter = CSVPrinter(writer, CSVFormat.DEFAULT
+            .withHeader("telefono","Mensaje", "Fecha y hora"));
+
+        for (sms in smsMessageList) {
+            val archivo = listOf(
+                sms.telefono,
+                sms.mensaje,
+                sms.fecha
+            )
+            csvPrinter.printRecord(archivo)
+        }
+        csvPrinter.flush()
+        csvPrinter.close()
+
+        Toast.makeText(this,"SE GUARDO",Toast.LENGTH_SHORT).show()
+        csv.clear()
+        listaSMSSeleccionados.clear()
+        binding.listaCVS.adapter = ArrayAdapter<String>(this, R.layout.simple_expandable_list_item_1,listaSMSSeleccionados)
+    }
+
+    private fun openCsvDocument() {
+        try {
+
+            // Obtenemos el file del path
+            val path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS
+            )
+
+            val fileName = "archivo.csv"
+            val file = File(path, "/$fileName")
+
+            // Abrimos el archivo
+            val csvIntent = Intent(Intent.ACTION_VIEW)
+            csvIntent.setDataAndType(
+                FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID + ".provider", file),
+                "text/csv"
+            )
+
+            csvIntent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION;
+            csvIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            this.startActivity(csvIntent)
+        } catch (e: java.lang.Exception) {
+            Toast.makeText(this, "Aplicación no encontrada. Asegurese de tener excel instalado", Toast.LENGTH_LONG).show()
+            e.printStackTrace()
+
         }
     }
 
